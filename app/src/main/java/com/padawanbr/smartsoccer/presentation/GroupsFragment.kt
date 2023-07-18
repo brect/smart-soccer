880091756a64285bf9a5e24099bab5b94dc3e29c
Bruno Lima <contato@blimas.dev>
2023-07-18 01:05:59 -0300

    ajusta navegação e refatora layouts

--- app/src/main/java/com/padawanbr/smartsoccer/presentation/GroupsFragment.kt
+++ app/src/main/java/com/padawanbr/smartsoccer/presentation/GroupsFragment.kt
@@ -1,13 +1,17 @@
 package com.padawanbr.smartsoccer.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.snackbar.Snackbar
 import com.padawanbr.smartsoccer.R
+import com.padawanbr.smartsoccer.databinding.BottonsheetCreateGroupBinding
 import com.padawanbr.smartsoccer.databinding.FragmentFirstBinding
+import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -19,6 +23,10 @@ class GroupsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var bottomSheetDialog: BottomSheetDialog
+    private var _bottomSheetBinding: BottonsheetCreateGroupBinding? = null
+    private val bottomSheetBinding: BottonsheetCreateGroupBinding get() = _bottomSheetBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,8 +41,31 @@ class GroupsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        bindingBottomSheetToCreateGroup()
+
+        binding.floatingActionButton.setOnClickListener {
+            bottomSheetDialog.show()
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAnchorView(R.id.floatingActionButton)
+                .setAction("Action", null).show()
+        }
+    }
+
+    private fun bindingBottomSheetToCreateGroup() {
+        // Crie um novo BottomSheetDialog aqui
+        bottomSheetDialog = BottomSheetDialog(requireContext())
+
+        // Inflate your custom layout with ViewBinding
+        _bottomSheetBinding = BottonsheetCreateGroupBinding.inflate(layoutInflater)
+
+        // Set the custom layout as the content view of the BottomSheetDialog
+        bottomSheetDialog.setContentView(bottomSheetBinding.root)
+
+        // Access elements in the layout using ViewBinding
+        bottomSheetBinding.buttonCreateGroup.setOnClickListener {
+            Toast.makeText(context, "Category buttonSaveCategory", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_GroupsFragment_to_SoccerPlayerFragment)
+            bottomSheetDialog.hide()
         }
     }
 
