2c9d0b2c9843a3ac7156a06ece1f2de496d46b85
Bruno Lima <contato@blimas.dev>
2023-07-30 18:33:38 -0300

    Ajusta logica para abrir fluxos de adicionar jogadores

--- app/src/main/java/com/padawanbr/smartsoccer/presentation/DetailsGroupFragment.kt
+++ app/src/main/java/com/padawanbr/smartsoccer/presentation/DetailsGroupFragment.kt
@@ -1,13 +1,22 @@
 package com.padawanbr.smartsoccer.presentation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.padawanbr.smartsoccer.R
 import com.padawanbr.smartsoccer.databinding.FragmentDetailsGroupBinding
+import com.padawanbr.smartsoccer.presentation.common.ViewAnimation.init
+import com.padawanbr.smartsoccer.presentation.common.ViewAnimation.rotateFab
+import com.padawanbr.smartsoccer.presentation.common.ViewAnimation.showIn
+import com.padawanbr.smartsoccer.presentation.common.ViewAnimation.showOut
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -18,6 +27,8 @@ class DetailsGroupFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args by navArgs<DetailsGroupFragmentArgs>()
+
+    var isRotate: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,12 +44,46 @@ class DetailsGroupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val groupName = args.detailsGroupViewArgs?.nome
+        groupName?.let { setToolbarTitle(it) }
 
-        binding.textViewSoccerPlayerGroupName.text = args.detailsGroupViewArgs?.nome
+        binding.textViewSoccerPlayerGroupName.text = groupName
 
-
+        configureFabMoreOptions()
     }
 
+    private fun setToolbarTitle(title: String) {
+        (activity as AppCompatActivity).supportActionBar?.title = title
+    }
+
+    private fun configureFabMoreOptions() {
+
+        init(binding.fabAddSoccerPlayer)
+        init(binding.textViewAddSoccerPlayer)
+
+        binding.fabMoreOptions.setOnClickListener {
+
+            isRotate = rotateFab(it, !isRotate)
+
+            if(isRotate){
+                showIn(binding.fabAddSoccerPlayer)
+                showIn(binding.textViewAddSoccerPlayer)
+            } else {
+                showOut(binding.fabAddSoccerPlayer)
+                showOut(binding.textViewAddSoccerPlayer)
+            }
+        }
+
+        binding.fabAddSoccerPlayer.setOnClickListener {
+            val directions = DetailsGroupFragmentDirections.actionDetailsGroupFragmentToSoccerPlayerFragment()
+
+            if (args.detailsGroupViewArgs != null) {
+                directions.groupId = it.id
+            }
+
+            findNavController().navigate(directions)
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
