ac96834087b4ce79ec61c8d3c9c6f5bee679c196
brect <contato@blimas.dev>
2023-08-23 00:21:59 -0300

    Ajusta implementação para ter timer picker

--- app/src/main/java/com/padawanbr/smartsoccer/presentation/ui/groups/DetailsGroupFragment.kt
+++ app/src/main/java/com/padawanbr/smartsoccer/presentation/ui/groups/DetailsGroupFragment.kt
@@ -6,23 +6,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.SeekBar
-import android.widget.SeekBar.OnSeekBarChangeListener
-import android.widget.Spinner
+import android.widget.TimePicker
 import android.widget.Toast
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.slider.RangeSlider
-import com.google.android.material.slider.Slider
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.padawanbr.smartsoccer.core.domain.model.DiaDaSemana
 import com.padawanbr.smartsoccer.core.domain.model.TipoEsporte
 import com.padawanbr.smartsoccer.databinding.BottonsheetCreateGroupBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.MessageFormat
+import java.util.Locale
 import kotlin.math.roundToInt
+
 
 @AndroidEntryPoint
 class DetailsGroupFragment : BottomSheetDialogFragment() {
@@ -32,6 +32,8 @@ class DetailsGroupFragment : BottomSheetDialogFragment() {
 
     private val viewModel: DetailsGroupViewModel by viewModels()
     private val sharedViewModel: SharedGroupsViewModel by activityViewModels()
+
+    private lateinit var timePicker: TimePicker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,23 +48,15 @@ class DetailsGroupFragment : BottomSheetDialogFragment() {
         initSpinnerGroupModalityAdapter()
         initSpinnerGroupGameDayAdapter()
 
-        binding.seekBarRatingAge.addOnChangeListener(RangeSlider.OnChangeListener { slider, value, fromUser ->
-            val values = binding.seekBarRatingAge.values
+        beginningOfTheGameTimerPickerListener()
+        seekBarRatingAgeListener()
+        buttonCreateGroupListener()
 
-            binding.textViewAgeValueFromLbl.text = "${values[0].roundToInt()} - anos"
-            binding.textViewAgeValueToLbl.text = "${values[1].roundToInt()} - anos"
+        observeUiState()
 
-            binding.textViewAgeValueFromLbl.text
+    }
 
-            Log.d("From", values[0].toString())
-            Log.d("T0", values[1].toString())
-
-            Log.d("slider", "$slider")
-            Log.d("value", "$value")
-            Log.d("fromUser", "$fromUser")
-        })
-
-
+    private fun buttonCreateGroupListener() {
         binding.buttonCreateGroup.setOnClickListener {
             val textNameGroup = binding.editTextTextInputGroupName.text.toString()
             val qtdTeam = binding.editTextNumberOfVacancies.text.toString().toInt()
@@ -81,9 +75,54 @@ class DetailsGroupFragment : BottomSheetDialogFragment() {
 
             Toast.makeText(context, "buttonSaveItem", Toast.LENGTH_SHORT).show()
         }
+    }
 
-        observeUiState()
+    private fun seekBarRatingAgeListener() {
+        binding.seekBarRatingAge.addOnChangeListener(RangeSlider.OnChangeListener { slider, value, fromUser ->
+            val values = binding.seekBarRatingAge.values
 
+            binding.textViewAgeValueFromLbl.text = "${values[0].roundToInt()} - anos"
+            binding.textViewAgeValueToLbl.text = "${values[1].roundToInt()} - anos"
+
+            binding.textViewAgeValueFromLbl.text
+
+            Log.d("From", values[0].toString())
+            Log.d("T0", values[1].toString())
+
+            Log.d("slider", "$slider")
+            Log.d("value", "$value")
+            Log.d("fromUser", "$fromUser")
+        })
+    }
+
+    private fun beginningOfTheGameTimerPickerListener() {
+        binding.textViewBeginningOfTheGame.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setInputMode(MaterialTimePicker.INPUT_MODE_CLOCK)
+                .setTitleText("Horário do jogo")
+                .build()
+            timePicker.addOnPositiveButtonClickListener {
+                binding.textViewBeginningOfTheGame.setText(
+                    MessageFormat.format(
+                        "{0}:{1}",
+                        String.format(
+                            Locale.getDefault(),
+                            "%02d",
+                            timePicker.hour
+                        ),
+                        String.format(
+                            Locale.getDefault(),
+                            "%02d",
+                            timePicker.minute
+                        )
+                    )
+                )
+            }
+            timePicker.show(requireFragmentManager(), "tag")
+        }
     }
 
     private fun initSpinnerGroupGameDayAdapter() {
