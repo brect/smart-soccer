051ea0e496ff3d658484b3156c39ecab2c2f16a6
brect <contato@blimas.dev>
2023-08-26 23:18:47 -0300

    Ajusta conversao para não dar exception

--- app/src/main/java/com/padawanbr/smartsoccer/presentation/extensions/UtilsExtension.kt
+++ app/src/main/java/com/padawanbr/smartsoccer/presentation/extensions/UtilsExtension.kt
@@ -3,6 +3,11 @@ package com.padawanbr.smartsoccer.presentation.extensions
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-fun Float.roundToTwoDecimalPlaces(): Float {
+
+fun Float?.roundToTwoDecimalPlaces(): Float {
+    if (this == null || this.isNaN()) {
+        return 0.00f
+    }
+
     return BigDecimal(this.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toFloat()
 }
